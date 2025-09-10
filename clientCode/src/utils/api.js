5fc5a4fdd3136eaf28a12c0c360064ae8584210e
anilkumar <anilkumar.adada@stackular.com>
2025-09-10 03:01:14 +0000

    Internal ALB Added

--- clientCode/src/utils/api.js
+++ clientCode/src/utils/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:8222';
+export const API_BASE = 'http://internal-Microservices-Internal-83094094.eu-north-1.elb.amazonaws.com';
 
 export async function apiFetch(path, { method = 'GET', headers = {}, body } = {}) {
   const token = localStorage.getItem('jwt_token');
