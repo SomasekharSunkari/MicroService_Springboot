241e4b15e48c564db8046da14b41a4e6b7d2f2a5
anilkumar <anilkumar.adada@stackular.com>
2025-09-10 08:53:11 +0000

    Changed Inside Navbar in client code

--- clientCode/src/components/Checkout.jsx
+++ clientCode/src/components/Checkout.jsx
@@ -45,7 +45,7 @@ const Checkout = ({ cartItems, onComplete }) => {
         }
       };
 
-      const response = await fetch('http://localhost:8222/api/v1/payments', {
+      const response = await fetch('http://public-ms-balancer-968609518.eu-north-1.elb.amazonaws.com/api/v1/payments', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
